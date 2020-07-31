c7c5d52edefd9081ffe11822181a7c05f896ce63
Abigayle Peterson <32112798+abigaylerose03@users.noreply.github.com>
2020-07-31 15:52:10 -0700

    Add files via upload

---
+++ isPrime.py
@@ -0,0 +1,31 @@
+def isPrime(n):
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    x = 3
+    while x * x <= n:
+        if n %x == 0 :
+            return False
+
+def solution(n):
+    ret_str = "2"
+    no = 3
+    index = 1
+    dig = 1
+    den = 10
+    while index <= n + 4:
+        if isPrime(no):
+            index += dig
+            ret_str += str(no)
+        no += 2
+        print("this is no:" + str(no))
+        if int(no / den) != 0:
+            den *= 10
+            dig += 1
+
+            
+    return ret_str[n:n+5]
+    
+print(solution(2))
+    
